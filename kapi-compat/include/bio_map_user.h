8946c6c28466928239e31f42611049758413cc12
Joel Becker <joel.becker@oracle.com>
2008-12-04 00:39:39 +0000

    oracleasm: Better compat file for bio_map_user()
    This only needs a define, not a function.
    
    
    
    git-svn-id: http://oss.oracle.com/projects/oracleasm/src/trunk@399 af2e7e45-1c97-403c-8c3e-045742b29be9

--- kapi-compat/include/bio_map_user.h
+++ kapi-compat/include/bio_map_user.h
@@ -1,14 +1,6 @@
 #ifndef KAPI_BIO_MAP_USER_H
 #define KAPI_BIO_MAP_USER_H
 
-#include <linux/bio.h>
-
-static struct bio *old_bio_map_user(struct request_queue *q, struct block_device *bdev,
-                                    unsigned long uaddr, unsigned int len, int write_to_vm,
-                                    gfp_t gfp_mask)
-{
-    return bio_map_user(q, bdev, uaddr, len, write_to_vm);
-}
-#define kapi_asm_bio_map_user old_bio_map_user
+#define kapi_asm_bio_map_user(a, b, c, d, e, f)         bio_map_user(a, b, c, d, e)
 
 #endif
