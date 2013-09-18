d368091e98dc43af2782064f80787d5fe97f57ff
Martin K. Petersen <martin.petersen@oracle.com>
2013-09-18 19:29:12 -0400

    oracleasm: Add support for new error return codes from block/SCSI
    Make sure we correctly handle the additional error codes returned by the
    I/O stack.
    
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- kernel/driver.c
+++ kernel/driver.c
@@ -1227,6 +1227,8 @@ static void asm_end_ioc(struct asm_request *r, unsigned int bytes_done,
 			break;
 
 		case -EIO:
+		case -ENODATA:
+		case -EREMOTEIO:
 			r->r_error = ASM_ERR_IO;
 			break;
 
@@ -1234,6 +1236,8 @@ static void asm_end_ioc(struct asm_request *r, unsigned int bytes_done,
 			r->r_error = asm_integrity_error(r);
 			break;
 
+		case -ENOLINK:
+		case -EBADE:
 		case -ENODEV:
 			r->r_error = ASM_ERR_NODEV;
 			r->r_status |= ASM_LOCAL_ERROR;
